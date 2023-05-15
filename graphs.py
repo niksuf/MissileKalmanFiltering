3f0cd59ae9cc980dc686b7967777e144a66dd179
Nanded <niksuf1@yandex.ru>
2023-05-16 00:51:14 +0300

    now plot_results in one window

--- graphs.py
+++ graphs.py
@@ -2,74 +2,77 @@ import matplotlib.pyplot as plt
 
 
 def plot_results(df1, df2, df3):
+    fig, axs = plt.subplots(nrows=3, ncols=4)
+
     # X position
     # 1st missile
-    plt.plot(df1.index, df1['x1'] / 20, label="x First missile")
-    plt.plot(df1.index, df1['z1'] / 20, label="z1 First missile")
-    plt.plot(df1.index, df1['xhat1'] / 20, label="x hat First missile")
+    axs[0][0].plot(df1.index, df1['x1'] / 20, label="x First missile")
+    axs[0][0].plot(df1.index, df1['z1'] / 20, label="z1 First missile")
+    axs[0][0].plot(df1.index, df1['xhat1'] / 20, label="x hat First missile")
+
     # 2nd missile
-    plt.plot(df2.index, df2['x1'] / 20, label="x Second missile")
-    plt.plot(df2.index, df2['z1'] / 20, label="z1 Second missile")
-    plt.plot(df2.index, df2['xhat1'] / 20, label="x hat Second missile")
+    axs[1][0].plot(df2.index, df2['x1'] / 20, label="x Second missile")
+    axs[1][0].plot(df2.index, df2['z1'] / 20, label="z1 Second missile")
+    axs[1][0].plot(df2.index, df2['xhat1'] / 20, label="x hat Second missile")
     # 3rd missile
-    plt.plot(df3.index, df3['x1'] / 20, label="x Third missile")
-    plt.plot(df3.index, df3['z1'] / 20, label="z1 Third missile")
-    plt.plot(df3.index, df3['xhat1'] / 20, label="x hat Third missile")
-    plt.legend()
-    plt.ylabel('position')
-    plt.xlabel('time')
-    plt.title('X position')
-    plt.show()
+    axs[2][0].plot(df3.index, df3['x1'] / 20, label="x Third missile")
+    axs[2][0].plot(df3.index, df3['z1'] / 20, label="z1 Third missile")
+    axs[2][0].plot(df3.index, df3['xhat1'] / 20, label="x hat Third missile")
+    # labels and legend
+    axs[0][0].set_title('X position')
 
     # Y Position
     # 1st missile
-    plt.plot(df1.index, df1['x2'] / 20, label="y First missile")
-    plt.plot(df1.index, df1['z2'] / 20, label="z2 First missile")
-    plt.plot(df1.index, df1['xhat2'] / 20, label="y hat First missile")
+    axs[0][1].plot(df1.index, df1['x2'] / 20, label="y First missile")
+    axs[0][1].plot(df1.index, df1['z2'] / 20, label="z2 First missile")
+    axs[0][1].plot(df1.index, df1['xhat2'] / 20, label="y hat First missile")
     # 2nd missile
-    plt.plot(df2.index, df2['x2'] / 20, label="y Second missile")
-    plt.plot(df2.index, df2['z2'] / 20, label="z2 Second missile")
-    plt.plot(df2.index, df2['xhat2'] / 20, label="y hat Second missile")
+    axs[1][1].plot(df2.index, df2['x2'] / 20, label="y Second missile")
+    axs[1][1].plot(df2.index, df2['z2'] / 20, label="z2 Second missile")
+    axs[1][1].plot(df2.index, df2['xhat2'] / 20, label="y hat Second missile")
     # 3rd missile
-    plt.plot(df3.index, df3['x2'] / 20, label="y Third missile")
-    plt.plot(df3.index, df3['z2'] / 20, label="z2 Third missile")
-    plt.plot(df3.index, df3['xhat2'] / 20, label="y hat Third missile")
-    plt.legend()
-    plt.ylabel('position')
-    plt.xlabel('time')
-    plt.title('Y position')
-    plt.show()
+    axs[2][1].plot(df3.index, df3['x2'] / 20, label="y Third missile")
+    axs[2][1].plot(df3.index, df3['z2'] / 20, label="z2 Third missile")
+    axs[2][1].plot(df3.index, df3['xhat2'] / 20, label="y hat Third missile")
+    # labels and legend
+    axs[0][1].set_title('Y position')
+    for i in range(3):
+        for j in range(2):
+            axs[i][j].legend()
+            axs[i][j].set_ylabel('position')
+            axs[i][j].set_xlabel('time')
 
     # X velocity
     # 1st missile
-    plt.plot(df1.index, df1['x1_dot'] / 20, label="x1_dot First missile")
-    plt.plot(df1.index, df1['xhat1_dot'] / 20, label="xhat1_dot First missile")
+    axs[0][2].plot(df1.index, df1['x1_dot'] / 20, label="x1_dot First missile")
+    axs[0][2].plot(df1.index, df1['xhat1_dot'] / 20, label="xhat1_dot First missile")
     # 2nd missile
-    plt.plot(df2.index, df2['x1_dot'] / 20, label="x1_dot Second missile")
-    plt.plot(df2.index, df2['xhat1_dot'] / 20, label="xhat1_dot Second missile")
+    axs[1][2].plot(df2.index, df2['x1_dot'] / 20, label="x1_dot Second missile")
+    axs[1][2].plot(df2.index, df2['xhat1_dot'] / 20, label="xhat1_dot Second missile")
     # 3rd missile
-    plt.plot(df3.index, df3['x1_dot'] / 20, label="x1_dot Third missile")
-    plt.plot(df3.index, df3['xhat1_dot'] / 20, label="xhat1_dot Third missile")
-    plt.legend()
-    plt.ylabel('speed')
-    plt.xlabel('time')
-    plt.title('X velocity')
-    plt.show()
+    axs[2][2].plot(df3.index, df3['x1_dot'] / 20, label="x1_dot Third missile")
+    axs[2][2].plot(df3.index, df3['xhat1_dot'] / 20, label="xhat1_dot Third missile")
+    # labels and legend
+    axs[0][2].set_title('X velocity')
 
     # Y velocity
     # 1st missile
-    plt.plot(df1.index, df1['x2_dot'] / 20, label="x2_dot First missile")
-    plt.plot(df1.index, df1['xhat2_dot'] / 20, label="xhat2_dot First missile")
+    axs[0][3].plot(df1.index, df1['x2_dot'] / 20, label="x2_dot First missile")
+    axs[0][3].plot(df1.index, df1['xhat2_dot'] / 20, label="xhat2_dot First missile")
     # 2nd missile
-    plt.plot(df2.index, df2['x2_dot'] / 20, label="x2_dot Second missile")
-    plt.plot(df2.index, df2['xhat2_dot'] / 20, label="xhat2_dot Second missile")
+    axs[1][3].plot(df2.index, df2['x2_dot'] / 20, label="x2_dot Second missile")
+    axs[1][3].plot(df2.index, df2['xhat2_dot'] / 20, label="xhat2_dot Second missile")
     # 3rd missile
-    plt.plot(df3.index, df3['x2_dot'] / 20, label="x2_dot Third missile")
-    plt.plot(df3.index, df3['xhat2_dot'] / 20, label="xhat2_dot Third missile")
-    plt.legend()
-    plt.ylabel('position')
-    plt.xlabel('time')
-    plt.title('Y velocity')
+    axs[2][3].plot(df3.index, df3['x2_dot'] / 20, label="x2_dot Third missile")
+    axs[2][3].plot(df3.index, df3['xhat2_dot'] / 20, label="xhat2_dot Third missile")
+    # labels and legend
+    axs[0][3].set_title('Y velocity')
+    for i in range(3):
+        for j in range(2, 4):
+            axs[i][j].legend()
+            axs[i][j].set_ylabel('speed')
+            axs[i][j].set_xlabel('time')
+
     plt.show()
 
 ######################################################################################################################
